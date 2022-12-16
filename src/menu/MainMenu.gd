f5cf6df9de2a35345f3eabf5b64449ff0712d894
algnor <algot.nordstrom@gmail.com>
2022-12-16 01:37:27 +0100

    added objects to map

--- src/menu/MainMenu.gd
+++ src/menu/MainMenu.gd
@@ -3,11 +3,11 @@ extends Control
 @onready var networking_menu = get_parent().get_node("Networking Menu")
 
 func _on_start_button_pressed():
-    visible = false
-    networking_menu.visible = true
+	visible = false
+	networking_menu.visible = true
 
 func _on_options_button_pressed():
-    get_tree().change_scene_to_file("res://assets/scenes/options_menu.tscn")
+	get_tree().change_scene_to_file("res://assets/scenes/options_menu.tscn")
 
 func _on_exit_button_pressed():
-    get_tree().quit()
+	get_tree().quit()
